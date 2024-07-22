3e937443291460640a60999df35df56b34d637c4
Anton Balashov <tyquat@gmail.com>
2024-07-22 19:07:08 +0300

    module_4_1 done

---
+++ fake_math.py
@@ -0,0 +1,2 @@
+def divide(first: int, second: int):
+    return "Ошибка" if second == 0 else first / second
---
+++ true_math.py
@@ -0,0 +1,4 @@
+from math import inf
+
+def divide(first: int, second: int):
+    return inf if second == 0 else first / second
