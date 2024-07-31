babe49d0014adfc670a129e2069173e474da7df6
Anton Balashov <tyquat@gmail.com>
2024-08-01 00:48:00 +0300

    module_5_1 done

---
+++ module_5_1.py
@@ -0,0 +1,16 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor: int):
+        if 0 <= new_floor <= house.number_of_floors:
+            for i in range(new_floor):
+                print(f"Перешли на {i + 1} этаж")
+        else:
+            print("Такого этажа не существует")
+
+
+house = House("ЖК Эльбрус", 30)
+house.go_to(10)
+
